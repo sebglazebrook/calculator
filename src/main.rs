c7cec4723e238c1eee2f6349b18d7deaf02073ed
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2017-03-29 11:54:18 +0000

    Basic reading of input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate rl_sys;
+
+use rl_sys::readline;
+use rl_sys::history::listmgmt;
+
+fn evaluate(input: String) -> String {
+    String::from("hello there")
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    loop {
+        let output: String = match readline::readline("") {
+            Ok(Some(s)) => evaluate(s),
+            Ok(None) => break,  // user entered ctrl-d
+            Err(e) => {
+                println!("{}", e);
+                continue;
+
+            }
+        };
+        println!("=> {}", output);
+
+        // Enables up/down arrow scrolling through history
+        listmgmt::add(&output).unwrap();
+
+    }
 }
